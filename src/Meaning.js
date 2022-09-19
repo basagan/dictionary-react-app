c30027e91818635f88de520c6536899d8d21965f
basagan <87991144+basagan@users.noreply.github.com>
2022-09-19 17:23:04 -0400

    correction of syntax

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,20 +4,18 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <p>
-        <em>{props.meaning.partOfSpeech}</em>
-        {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <p>
-                {definition.definition}
-                <br />
-                <em>{definition.example}</em>
-              </p>
-            </div>
-          );
-        })}
-      </p>
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
